9f78916d175f8d0c4429efff549fae61cd524169
Trevor Sutcliffe <mrtsut@hotmail.com>
2018-01-18 15:16:20 -0600

    added sortedDictionary

--- Class2_Studio/Program.cs
+++ Class2_Studio/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Class2_Studio
 {
     class Program
     {
-        static void Main(string[] args)
+        /*public static int CharCount(string text, char c)
         {
-            Console.WriteLine("Hello World!");
+            int count = 0;
+            foreach (char ch in text)
+            {
+                if (ch.Equals(c))
+                {
+                    count++;
+                }
+
+            }
+            return count;
+        }*/
+
+        public static SortedDictionary<char, ulong> Count(string stringToCount)
+        {
+            SortedDictionary<char, ulong> characterCount = new SortedDictionary<char, ulong>();
+
+            foreach (var character in stringToCount)
+            {
+                if (!characterCount.ContainsKey(character))
+                {
+                    characterCount.Add(character, 1);
+                }
+                else
+                {
+                    characterCount[character]++;
+                }
+            }
+
+            return characterCount;
+           
+}
+
+            static void Main(string[] args)
+        {
+
+            string text = "Hello";
+            //int count = CharCount(text, 'l');
+
+            foreach (var character in Program.Count(text))
+            {
+                Console.WriteLine("{0} - {1}", character.Key, character.Value);
+            }
+        
+
+       // Console.WriteLine(count);
+            Console.ReadLine();
         }
     }
 }
